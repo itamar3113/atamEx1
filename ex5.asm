51ab3e85428fdebf3b194d7ca8737bfb9010a7ab
IdoDesign <IdoDesignil@gmail.com>
2023-05-04 20:00:27 +0300

    fixed ex5

--- ex5.asm
+++ ex5.asm
@@ -2,36 +2,37 @@
 
 .section .text
 _start:
-	movq new_node, %rcx # rcx = val
-	movq root, %rax # rax = root
+	movq new_node, %rcx # rcx = new_val
+	movq root, %rax     # rax = root
     test %rax, %rax
     jne L1
     movq $new_node, root
-    jmp end
+    jmp END_EX5_HW1
 	
 L1:	
-	movq (%rax), %rbx # rbx =  -> value
-	cmp %rbx, %rcx
-	jg L2
-	je end # need to check if possible if not don another comp
-	movq 8(%rax), %rdx # rdx = lson
-    test %rdx, %rdx
-    je L3
-    movq %rdx, %rax
+	movq (%rax), %rbx  # rbx = node -> value
+	cmp %rbx, %rcx     # if new_val == node -> value
+	jg GO_RIGHT_HW1              # if new_val > node -> value - go right
+    cmp %rbx, %rcx 
+	je END_EX5_HW1             # if new_val ==  node -> value - go to END_EX5_HW1
+	movq 8(%rax), %rdx #    %rdx = node -> lson
+    test %rdx, %rdx    # if %rdx == null insert node
+    je INSERT_LEFT_HW1
+    movq %rdx, %rax     # node = node -> lson
     jmp L1
 	
-L2:
-    movq 16(%rax), %rax # rdx = rson
+GO_RIGHT_HW1:
+    movq 16(%rax), %rdx # rdx = rson
     test %rdx, %rdx     # if rdx != null
-    je L4
+    je INSERT_RIGHT_HW1
     movq %rdx, %rax
     jmp L1
 
-L3:
-    movq %new_node, 8(%rax)
-    jmp end
+INSERT_LEFT_HW1:
+    movq $new_node, 8(%rax)
+    jmp END_EX5_HW1
 	
-L4:
+INSERT_RIGHT_HW1:
 	movq $new_node, 16(%rax)
  
- end:
+END_EX5_HW1:
