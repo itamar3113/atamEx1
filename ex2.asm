4bd31a5ab2fd2306c407889d7df522f591df7fe1
IdoDesign <IdoDesignil@gmail.com>
2023-05-09 21:27:30 +0300

    ex2 work

--- ex2.asm
+++ ex2.asm
@@ -2,33 +2,49 @@
 
 .section .text
 _start:
-#your code here
-	mov (num), %eax
-	cmp $0, %eax
-	js negative_HW1
-	xor %ebx, %ebx
-	movq destination, %rdi
-loop_HW1:
-	lea source(%ebx), %rsi
-	cmp %rdi, %rsi
-	je shift_dest_HW1
-	cmp $1, %eax
-	jl end_HW1
-	movb source(%ebx), %r10b 
-	movb %r10b, destination(%ebx)
-	add $1, %ebx
-	sub $1, %eax
-	jmp loop_HW1
-	negative_HW1:
-	mov %eax, (destination)
-shift_dest_HW1:
-	lea (%eax, %ebx, -1), %ecx
-	cmp $1, %eax
-	jl end_HW1
-	movb source(%ebx), %r10b 
-	movb %r10b, destination(%ecx)
-	add $1, %ebx
-	add $1, %ecx
-	sub $1, %eax
-	jmp loop_HW1
-end_HW1:
+    leaq source, %rsi
+    leaq destination, %rdi
+    mov num, %ecx
+    cmp $0, %ecx
+    js negative_HW1
+    xor %rbx, %rbx
+    xor %rdx, %rdx
+    
+    movsx %ecx, %rcx
+    cmp %rsi, %rdi
+    jle no_overlap_HW1
+    
+    mov %rdi, %rax
+    sub %rsi, %rax
+    cmp %rcx, %rax
+    jge no_overlap_HW1
+    
+    leaq (%rsi, %rcx), %r8
+    leaq (%rdi, %rcx), %r9
+    sub $1, %r8
+    sub $1, %r9
+
+goback_HW1:
+    movb (%r8), %r10b
+    movb %r10b, (%r9)
+    sub $1, %r8
+    sub $1, %r9
+    dec %rcx
+    cmp $0, %rcx
+    jg goback_HW1
+    jmp end_ex2_HW1 
+
+no_overlap_HW1:
+    movb (%rsi, %rdx), %r9b
+    movb %r9b, (%rdi, %rdx)
+    inc %rdx
+    dec %rcx
+    cmp $0, %rcx
+    jg no_overlap_HW1
+    jmp end_ex2_HW1
+
+negative_HW1:
+    movl (num), %eax
+    movl %eax, destination
+    
+end_ex2_HW1:
