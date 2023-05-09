00a0cff581ddae9441773f7e0065271355d2bc2d
itamar3113 <itamar3113@gmail.com>
2023-05-09 18:33:05 +0300

    ex2 fix

--- ex2.asm
+++ ex2.asm
@@ -7,40 +7,28 @@ _start:
 	cmp $0, %eax
 	js negative_HW1
 	xor %ebx, %ebx
+	movq destination, %rdi
 loop_HW1:
-	cmp $8, %eax
-	jge quad_HW1
-	cmp $4, %eax
-	jge double_HW1
-	cmp $2, %eax
-	jge word_HW1
+	lea source(%ebx), %rsi
+	cmp %rdi, %rsi
+	je shift_dest_HW1
 	cmp $1, %eax
-	jge byte_HW1
-	jmp end_HW1
-quad_HW1:
-	movq source(%ebx), %r10 
-	movq %r10, destination(%ebx)
-	add $8, %rbx
-	sub $8, %eax
-	jmp loop_HW1
-double_HW1:
-	movl source(%ebx), %r10d 
-	movl %r10d, destination(%ebx)
-	add $4, %rbx
-	sub $4, %eax
-	jmp loop_HW1
-word_HW1:
-	movw source(%ebx), %r10w 
-	movw %r10w, destination(%ebx)
-	add $2, %rbx
-	sub $2, %eax
-	jmp loop_HW1
-byte_HW1:
+	jl end_HW1
 	movb source(%ebx), %r10b 
 	movb %r10b, destination(%ebx)
-	add $1, %rbx
+	add $1, %ebx
 	sub $1, %eax
 	jmp loop_HW1
 	negative_HW1:
 	mov %eax, (destination)
+shift_dest_HW1:
+	lea (%eax, %ebx, -1), %ecx
+	cmp $1, %eax
+	jl end_HW1
+	movb source(%ebx), %r10b 
+	movb %r10b, destination(%ecx)
+	add $1, %ebx
+	add $1, %ecx
+	sub $1, %eax
+	jmp loop_HW1
 end_HW1:
