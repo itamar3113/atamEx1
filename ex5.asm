56b256f58d1d0a559b6eda44f799c93c2a5f31de
itamar3113 <itamar3113@gmail.com>
2023-05-09 21:48:30 +0300

    ex5

--- ex5.asm
+++ ex5.asm
@@ -5,11 +5,11 @@ _start:
 	movq new_node, %rcx # rcx = new_val
 	movq root, %rax     # rax = root
     test %rax, %rax
-    jne L1
+    jne L1_HW1
     movq $new_node, root
     jmp END_EX5_HW1
 	
-L1:	
+L1_HW1:	
 	movq (%rax), %rbx  # rbx = node -> value
 	cmp %rbx, %rcx     # if new_val == node -> value
 	jg GO_RIGHT_HW1              # if new_val > node -> value - go right
@@ -26,7 +26,7 @@ GO_RIGHT_HW1:
     test %rdx, %rdx     # if rdx != null
     je INSERT_RIGHT_HW1
     movq %rdx, %rax
-    jmp L1
+    jmp L1_HW1
 
 INSERT_LEFT_HW1:
     movq $new_node, 8(%rax)
